bf343ad6d94e3dd5f4685a0b5d090576c57ac7a1
Anil Jaiswal <er.anil.jaiswal@gmail.com>
2016-10-14 15:32:55 +0530

    Footer and contact page updates

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,7 @@
 /* Add here all your JS customizations */
+var d = new Date();
+var currentYear = d.getFullYear();
+
+$(document).ready(function(e){
+    $('body > div > footer > div.footer-copyright > div > div > div:nth-child(2)').text('© Copyright '+ currentYear +' JE Infratech.')
+});
